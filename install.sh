c276ba0e0e1a9d1a6bad4ade45d3b260d8512727
Klaas Hofman <Klaas.hofman@essensium.com>
2014-08-18 22:06:17 +0200

    dgen linux version

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
+# ANSI color codes
+RS='\e[0m'    	# reset
+FRED='\e[31m' 	# foreground red
+BRED='\e[1;31m' # bold red
+
+echo -e "$BRED --- DGEN --- $RS"
 # install prerequisites
-sudo apt-get install libsdl1.2-dbg libsdl1.2-dev libsdl1.2debian
+sudo apt-get install --assume-yes libsdl1.2-dbg libsdl1.2-dev libsdl1.2debian
 # dgen-sdl-x86 - genesis emulator
-rm -rf dgen-sdl-1.33-w32-x86+SSE2
-wget http://sourceforge.net/projects/dgen/files/dgen/1.33/dgen-sdl-1.33-w32-x86%2BSSE2.zip
-unzip dgen-sdl-1.33-w32-x86+SSE2.zip
-cd dgen-sdl-1.33-w32-x86+SSE2
-./configure || exit "dgen: configure error"
-make && sudo make install
+rm -rf dgen-sdl-1.33.tar.gz
+wget http://sourceforge.net/projects/dgen/files/dgen/1.33/dgen-sdl-1.33.tar.gz
+tar xvzf dgen-sdl-1.33.tar.gz
+cd dgen-sdl-1.33
+./configure || exit "$FRED dgen: configure error $RS"
+make && sudo make install || exit "$FRED dgen: error building $RS"
 
 # frontend application
 
